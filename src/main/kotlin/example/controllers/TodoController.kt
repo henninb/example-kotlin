227070bbc295313d1f332d861f3c66cd1afaa567
Brian Henning <henninb@msn.com>
2022-12-23 18:08:35 -0600

    wip

--- src/main/kotlin/example/controllers/TodoController.kt
+++ src/main/kotlin/example/controllers/TodoController.kt
@@ -2,13 +2,12 @@ package example.controllers
 
 import example.domain.Todo
 import example.services.TodoService
-import jakarta.ws.rs.core.MediaType
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
-
 
 @Controller
 class TodoController(private var todoService: TodoService) {
@@ -17,24 +16,19 @@ class TodoController(private var todoService: TodoService) {
     fun index(): ModelAndView {
         val mav = ModelAndView("index")
         val dataList = todoService.findAllTodos()
-        println(dataList)
-        //mav.addObject("todos", mapOf("todos" to dataList))
         mav.addObject("todos", dataList)
         return mav
     }
 
-    @RequestMapping(value = ["/add"], method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_FORM_URLENCODED])
+    @RequestMapping(value = ["/add"], method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun add(@RequestParam("text") request: String) :String {
-      println(request)
       val todo = Todo(0, request)
       todoService.save(todo)
-      println("add value")
       return "redirect:/index";
     }
 
-    @RequestMapping(value = ["/delete"], method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_FORM_URLENCODED])
+    @RequestMapping(value = ["/delete"], method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun delete(@RequestParam("id") id: Long) :String {
-        //println(request)
         todoService.delete(id)
         return "redirect:/index";
     }
